5c248108ba030c834eeff5491b8b5e4b12cc6f2b
Jacek Wikiera <jf@wikiera.net>
2020-03-22 13:16:24 +0100

    Update variables.py

--- variables.py
+++ variables.py
@@ -1,5 +1,10 @@
-TOKEN = "***"
-DEBUG = False
-DEFAULT_PREFIX = "!"
-DB_FILENAME = "alphadb.json"
-WHITELIST_FILENAME = "whitelist.json"
+import pprint
+
+TOKEN = "***" #Unique discord application token
+DEBUG = True #True/False, if debug messages should appear in the console
+DEFAULT_PREFIX = "!" #Default prefix the bot ûses for commands
+DB_FILENAME = "alphadb.json" #For example "alphadb.json"
+WHITELIST_FILENAME = "whitelist.json" #whitelist.json until Mojang makes a political correctness update
+
+#?? must check if true ?? #Instantiation of a PrettyPrinter object. "pp.pformat(<object>)" converts objects like lists or dicts to string 
+pp = pprint.PrettyPrinter(indent=4)
